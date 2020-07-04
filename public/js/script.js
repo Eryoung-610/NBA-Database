0da7932c605be79284c1144e826db4c242345d53
Eryoung-610 <eryoung610@gmail.com>
2020-07-04 16:58:52 -0700

    Created modal for login

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.parallax');
+//     var instances = M.Parallax.init(elems, options);
+//   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
   });
